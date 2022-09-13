fe0ca7e93246d4e016a471b40b804756dc5991b2
Robert Njoroge <mainarobert961@gmail.com>
2022-09-13 05:51:52 -0700

    0putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry
+ *
+ * Description: printst putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	int letter;
+	letter = '_';
+	putchar(letter);
+	letter = 'p';
+	putchar(letter);
+	letter = 'u';
+	putchar(letter);
+	letter = 't';
+	putchar(letter);
+	letter = 'c';
+	putchar(letter);
+	letter = 'h';
+	putchar(letter);
+	letter = 'a';
+	putchar(letter);
+	letter = 'r';
+	putchar(letter);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+0-putchar.c
+ a program that prints _putchar, followed by a new line
