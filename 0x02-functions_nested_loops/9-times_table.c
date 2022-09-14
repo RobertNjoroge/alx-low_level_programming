a42923e451e962b833805a9f60b24c2d26d252e9
Robert Njoroge <mainarobert961@gmail.com>
2022-09-14 06:30:22 -0700

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,30 +5,30 @@
  */
 void times_table(void)
 {
-	int i, j, r;
+	int i, j, res;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			r = (i * j);
+			res = (i * j);
 			if (j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
-			if (r >= 10)
+			if (res >= 10)
 			{
-				_putchar((r / 10) + '0');
-				_putchar((r % 10) + '0');
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
 			}
-			else if (r < 10 && j != 0)
+			else if (res < 10 && j != 0)
 			{
 				_putchar(' ');
-				_putchar((r % 10) + 0);
+				_putchar((res % 10) + 0);
 			}
 			else
-				_putchar((r % 10) + '0');
+				_putchar((res % 10) + '0');
 		}
 		_putchar('\n');
 	}
