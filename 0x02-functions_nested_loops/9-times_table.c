fd3828aeb01d3fb11253114986afdfea0b652ef0
Robert Njoroge <mainarobert961@gmail.com>
2022-09-14 06:14:40 -0700

    timetable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * times_table - prints time table from 0-9
+ * Return: None
+ */
+void times_table(void)
+{
+	int i, j, r;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			r = (i * j);
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (r >= 10)
+			{
+				_putchar((r / 10) + '0');
+				_putchar((r % 10) + '0');
+			}
+			else if (r < 10 && j != 0)
+			{
+				_putchar(' ');
+				_putchar((r % 10) + 0);
+			}
+			else
+			{
+				_putchar((r % 10) + '0');
+			}
+		}
+	}
