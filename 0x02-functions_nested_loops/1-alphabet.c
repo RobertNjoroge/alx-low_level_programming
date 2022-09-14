61fb306f12abab93f7e54e2ffc9ed7cacf1030af
Robert Njoroge <mainarobert961@gmail.com>
2022-09-14 00:49:16 -0700

    alphabet1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_alphabet - This program prints the english alphabets a-z
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 #ifndef HEADER_FILE
 #define HEADER_FIE
-
+void print_alphabet(void);
 
 
 #endif
