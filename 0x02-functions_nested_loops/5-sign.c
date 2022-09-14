78a797f6c1d16548a4c2253343a0f36bb69b0458
Robert Njoroge <mainarobert961@gmail.com>
2022-09-14 00:17:11 -0700

    alphabets

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,22 +8,22 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	}
 	if (n == 0)
 	{
-		putchar('0');
+		_putchar('0');
 		return (0);
 	}
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		return (-1);
 	}
 	else
 	{
-		putchar('/');
+		_putchar('/');
 		return ('/');
 	}
 }
