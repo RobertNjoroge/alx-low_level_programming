2c9f148d0522a27dcd0d1ffd001e87914885beba
Robert Njoroge <mainarobert961@gmail.com>
2022-09-19 05:53:58 -0700

    arrays and pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: String to reverse
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int len = strlen(s);
+
+	while (len --)
+		putchar(*(s + len));
+	putchar(10);
+}
