21df63f3b9d4f586e3956183d480e5f57441b123
Robert Njoroge <mainarobert961@gmail.com>
2022-09-21 10:54:09 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -24,6 +24,11 @@ int _strcmp(char *s1, char *s2)
 			diff = s2[i];
 			break;
 		}
+		else if (s2[i] == '\0')
+		{
+			diff = s1[i];
+			break;
+		}
 		else if (s[i] != s2[i])
 		{
 			diff = s1[i] = s2[i];
