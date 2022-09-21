734fa7c433c97c0e14f47798c5df38c2d852c4a4
Robert Njoroge <mainarobert961@gmail.com>
2022-09-21 10:12:14 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,8 +11,13 @@
 
 char *_strcat(char *dest, char *src)
 {
-	char *ptr;
+	int index = 0, dest_len = 0;
 
-	ptr = (dest, src);
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
 }
