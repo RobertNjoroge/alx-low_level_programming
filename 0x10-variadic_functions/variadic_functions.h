85ee622dc87ab2fdbd448fac27bcaca21235467c
Robert Njoroge <mainarobert961@gmail.com>
2022-10-14 02:18:48 -0700

    alphabets 2

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,19 @@
 
 #include<stdio.h>
 #include<stdarg.h>
+
+/**
+ * struct printer - a new strust type defining a printer
+ * @symbol: a symbol representing a data type
+ * @print: a function pointer to afunction
+ */
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+
+}
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
