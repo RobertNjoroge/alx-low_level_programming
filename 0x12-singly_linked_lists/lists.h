a82fd22d0f6021506d9430a085a07e43ad2c0dfa
Robert Njoroge <mainarobert961@gmail.com>
2022-10-24 02:34:04 -0700

    singly

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,7 +5,7 @@
 #include<string.h>
 
 /**
- * struct lists_s - singly linked list
+ * struct list_s - singly linked list
  * @str: string: - string that is malloced
  * @len: length of the string
  * @next: points to the next node
@@ -19,7 +19,6 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
