77bc890a027e6eb4bb68126644c61cb17e2bb9aa
NjorogeRobert <mainarobertn@gmail.com>
2022-12-13 04:15:44 -0800

    libdynamic.so shared library

---
+++ 0x18-dynamic_libraries/strcat.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+/**
+ * _strcat - combines two strings
+ * @dest: first string
+ * @src: second string
+ *
+ * Return: Always Success
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (0);
+}
